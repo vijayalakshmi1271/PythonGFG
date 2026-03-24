f64123fa9ff533c560011b255d585140ccbefa83
Vijaya Lakshmi Yarramsetti <vijaya123laxmi143@gmail.com>
2026-03-24 21:14:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Subarray with Sum K/longest-subarray-with-sum-k.py
+++ Difficulty: Medium/Longest Subarray with Sum K/longest-subarray-with-sum-k.py
@@ -1,14 +1,22 @@
 class Solution:
-    def longestSubarray(self, arr, k):  
+    def longestSubarray(self, arr, k):
         prefix_sum = 0
         max_len = 0
-        seen = {}
+        prefix_map = {}
+
         for i in range(len(arr)):
             prefix_sum += arr[i]
+
+            # Case 1: if prefix sum itself equals k
             if prefix_sum == k:
                 max_len = i + 1
-            if prefix_sum - k in seen:
-                max_len = max(max_len, i - seen[prefix_sum - k])
-            if prefix_sum not in seen:
-                seen[prefix_sum] = i
+
+            # Case 2: if (prefix_sum - k) exists
+            if (prefix_sum - k) in prefix_map:
+                max_len = max(max_len, i - prefix_map[prefix_sum - k])
+
+            # Store only first occurrence
+            if prefix_sum not in prefix_map:
+                prefix_map[prefix_sum] = i
+
         return max_len
